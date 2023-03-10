1c3c5bb12f0ecacdd9026b63aaf4ec3abfe20faa
William Young <wifryo@gmail.com>
2023-03-10 07:36:07 +0100

    Continue prep

---
+++ Topics/NPE/Avoid NPE with strings/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static String concatStrings(String str1, String str2) {
+        String string1 = str1 != null ? str1 : "";
+        String string2 = str2 != null ? str2 : "";
+
+        return string1.concat(string2);
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        str1 = "null".equalsIgnoreCase(str1) ? null : str1;
+        str2 = "null".equalsIgnoreCase(str2) ? null : str2;
+
+        System.out.println(concatStrings(str1, str2));
+    }
+}
