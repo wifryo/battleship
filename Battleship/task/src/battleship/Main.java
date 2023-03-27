0a88a76667c0829cb0f914e5d978eb639be8430d
William <wifryo@gmail.com>
2023-03-27 14:25:56 +0200

    Add support for second player WIP

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -1,5 +1,6 @@
 package battleship;
 import java.lang.*;
+import java.util.Scanner;
 
 import static java.lang.Math.abs;
 
@@ -12,25 +13,53 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Board board = new Board();
-        board.draw();
+        Board board1 = new Board();
+        board1.draw();
 
         for (Ship ship : Ship.values()) {
-            Position[] inputPositions = board.getShipPosition(ship);
-            board.addShipToBoard(inputPositions[0], inputPositions[1]);
-            board.draw();
+            Position[] inputPositions = board1.getShipPosition(ship);
+            board1.addShipToBoard(inputPositions[0], inputPositions[1]);
+            board1.draw();
         }
-        System.out.println("The game starts!");
-        board.drawFog();
-        System.out.println();
 
-        while (board.doShipsRemain()) {
-            board.takeShot();
-            board.draw();
+        System.out.println("Press Enter and pass the move to another player");
+        Scanner scanner = new Scanner(System.in);
+        String pos1 = scanner.next();
+
+        Board board2 = new Board();
+        board2.draw();
+
+        for (Ship ship : Ship.values()) {
+            Position[] inputPositions = board2.getShipPosition(ship);
+            board2.addShipToBoard(inputPositions[0], inputPositions[1]);
+            board2.draw();
+        }
+
+        System.out.println("Press Enter and pass the move to another player");
+        pos1 = scanner.next();
+
+        while (board1.doShipsRemain() && board2.doShipsRemain()) {
+            board2.drawFog();
+            System.out.println("---------------------");
+            board1.draw();
             System.out.println();
-        }
+            System.out.println("Player 1, it's your turn:");
+            board2.takeShot();
+            if (!board2.doShipsRemain()) {
+                System.out.println("You sank the last ship. You won. Congratulations!");
+            }
 
-        System.out.println("You sank the last ship. You won. Congratulations!");
+            board1.drawFog();
+            System.out.println("---------------------");
+            board2.draw();
+            System.out.println();
+            System.out.println("Player 2, it's your turn:");
+            board1.takeShot();
+            if (!board1.doShipsRemain()) {
+                System.out.println("You sank the last ship. You won. Congratulations!");
+            }
+        }
+//
 
     }
 }
